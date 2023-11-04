0aad32740e7fef1e24cff4c65aa6f6c6bc85d1a6
Lavanya <lavanyaj0725@gmail.com>
2023-11-04 09:26:55 +0530

    daisycode

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Register from './Components/Register';
+import './style.css';
+
+function App(){
+  return(
+    <div className="App">
+      <Register />
+    </div>
+  )
+}
+
+export default App;
+
+
+
