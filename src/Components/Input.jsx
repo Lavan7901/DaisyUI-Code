3f0dcb4527da4ad62e1bdb60401f7f2ce0140e47
Lavanya <lavanyaj0725@gmail.com>
2023-11-07 17:35:09 +0530

    changes

--- src/Components/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Input (props){
-    return(
-        <input className="input input-bordered w-80 max-w-xs" type={props.type} 
-         placeholder={props.placeholder} onChange={props.onChange} />
-    )
-}
-export default Input;
